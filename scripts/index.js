64c78451007c4cdf30d2e3c7e5b971db01391938
LorangeJuicey <60689696+LorangeJuicey@users.noreply.github.com>
2022-10-04 09:10:24 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var MoneyCheckbox = document.querySelector(".money-slider");
+  var DistanceCheckbox = document.querySelector(".distance-slider");
+
+  MoneyCheckbox.addEventListener("change", function () {
+    if (MoneyCheckbox.checked) {
+      $(document).ready(function () {
+        $(".money").addClass("burgers");
+        $(".money").removeClass("metric");
+        MoneyConvert(0);
+      });
+    } else {
+      $(".money").removeClass("burgers");
+      $(".money").addClass("metric");
+      MoneyConvert(1);
+    }
+  });
+  DistanceCheckbox.addEventListener("change", function () {
+    if (DistanceCheckbox.checked) {
+      $(document).ready(function () {
+        $(".distance").addClass("burgers");
+        $(".distance").removeClass("metric");
+      });
+    } else {
+      $(".distance").removeClass("burgers");
+      $(".distance").addClass("metric");
+    }
+  });
+});
+
+function MoneyConvert(convert) {
+  if (convert == 0) {
+    let tempMoney = document.getElementById("huidige-kosten").innerHTML;
+    if (tempMoney != 0) {
+      tempMoney = tempMoney * 1.02;
+      document.getElementById("huidige-kosten").innerHTML = tempMoney;
+    }
+
+    tempMoney = document.getElementById("geschatte-kosten").innerHTML;
+    if (tempMoney != 0) {
+      tempMoney = tempMoney * 1.02;
+      document.getElementById("geschatte-kosten").innerHTML = tempMoney;
+    }
+
+    tempMoney = document.getElementById("kosten").innerHTML;
+    if (tempMoney != 0) {
+      tempMoney = tempMoney * 1.02;
+      document.getElementById("kosten").innerHTML = tempMoney;
+    }
+  } else if (convert == 1) {
+    let tempMoney = document.getElementById("huidige-kosten").innerHTML;
+    if (tempMoney != 0) {
+      tempMoney = tempMoney / 1.02;
+      document.getElementById("huidige-kosten").innerHTML = tempMoney;
+    }
+
+    tempMoney = document.getElementById("geschatte-kosten").innerHTML;
+    if (tempMoney != 0) {
+      tempMoney = tempMoney / 1.02;
+      document.getElementById("geschatte-kosten").innerHTML = tempMoney;
+    }
+
+    tempMoney = document.getElementById("kosten").innerHTML;
+    if (tempMoney != 0) {
+      tempMoney = tempMoney / 1.02;
+      document.getElementById("kosten").innerHTML = tempMoney;
+    }
+  }
+}
